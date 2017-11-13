9c56d10119149eefc0fe5571916bb92ff5942122
soulquiz <soulquiza@gmail.com>
2017-11-14 00:42:20 +0700

    add username password in header for authen

--- savon_client.rb
+++ savon_client.rb
@@ -1,31 +1,31 @@
 require 'savon'
-# client = Savon.client(wsdl: "http://localhost:8080/CalculatorWS/CalculatorWS?wsdl")
-# client = Savon.client(wsdl: "http://localhost:8084/secure/secure?wsdl")
-# client = Savon.client(wsdl: "http://www.yenaura.com/ECEItemWS?wsdl")
-# client = Savon.client(wsdl: "http://13.115.108.102/ECEItemWS?wsdl")
-client = Savon.client(wsdl: "http://localhost:8080/testauthen/testauthen?wsdl", headers: { "Username" => "happysoap", "Password" => "1234"})
 
+client = Savon.client(wsdl: "https://www.yenaura.com/ECEItemWS?wsdl", headers: { "Username" => "happysoap", "Password" => "1234"})
 
-message = {name: "chatchai" , Username: "happysoap", Password: "1234"}
-response = client.call(:hello,message: message)
-# puts response
+## display hello chatchai
+response = client.call(:hello, message: {name: "chatchai"})
 puts response.to_hash[:hello_response][:return]
-# i = 10
-# j = 2
-# message = {i: i, j: j}
 
-# response = client.call(:add, message: message).to_hash[:add_response][:return]
-# print i , " + " , j , " = " , response  # 10 + 2 = 12
-# puts ""
-#
-# response = client.call(:subtraction, message: message).to_hash[:subtraction_response][:return]
-# print i , " - " , j , " = " , response  # 10 - 2 = 8
-# puts ""
-#
-# response = client.call(:multiply, message: message).to_hash[:multiply_response][:return]
-# print i , " * " , j , " = " , response  # 10 * 2 = 20
-# puts ""
-#
-# response = client.call(:divide, message: message).to_hash[:divide_response][:return]
-# print i , " / " , j , " = " , response  # 10 / 2 = 5
-# puts ""
+## display order 5
+response = client.call(:get_order, message: {orderNumber: "5"})
+result = response.to_hash[:get_order_response][:return]
+puts result
+# File.write('D:\Downloads\getOrder5.xml', result)
+
+## display examine info order 5
+response = client.call(:get_examine_info, message: {orderNumber: "5"})
+result = response.to_hash[:get_examine_info_response][:return]
+puts result
+# File.write('D:\Downloads\getExamineInfo.xml', result)
+
+## display last order
+response = client.call(:get_last_order)
+result = response.to_hash[:get_last_order_response][:return]
+puts result
+# File.write('D:\Downloads\getLastOrder.xml', result)
+
+## display take info order 5
+response = client.call(:get_take_info, message: {orderNumber: "5"})
+result = response.to_hash[:get_take_info_response][:return]
+puts result
+# File.write('D:\Downloads\getTakeInfo.xml', result)
